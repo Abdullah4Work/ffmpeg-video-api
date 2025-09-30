442662bdac47bb7a0e4cc9468955e9e975a9d40a
Abdullah Omar <154799427+Abdullah4Work@users.noreply.github.com>
2025-09-30 16:22:07 +0300

    Update Video.js

--- src/Video.js
+++ src/Video.js
@@ -1,1 +1,19 @@
+import { AbsoluteFill, Audio, Sequence, Img, useVideoConfig } from "remotion";
 
+export const MyVideo: React.FC<{
+  audioUrl: string;
+  imageUrl: string;
+  caption: string;
+}> = ({ audioUrl, imageUrl, caption }) => {
+  const { durationInFrames, fps } = useVideoConfig();
+
+  return (
+    <AbsoluteFill style={{ backgroundColor: "black" }}>
+      <Img src={imageUrl} style={{ objectFit: "cover", width: "100%", height: "100%" }} />
+      <Audio src={audioUrl} />
+      <AbsoluteFill style={{ justifyContent: "center", alignItems: "center" }}>
+        <h1 style={{ color: "white", fontSize: 60, textAlign: "center" }}>{caption}</h1>
+      </AbsoluteFill>
+    </AbsoluteFill>
+  );
+};
